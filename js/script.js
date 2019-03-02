705e30ff0c05c695782c6cbc7c25ab88e8394a91
AhlemSalem <47692958+AhlemSalem@users.noreply.github.com>
2019-03-02 14:39:28 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// function funn(){
+//     var x=document.getElementById("T");
+//     x.style.fontStyle='italic';
+// }
+// function funnU(){
+//     var x=document.getElementById("T");
+//     x.style.textDecoration='underline';
+// }
+// function funnB(){
+//     var x=document.getElementById("T");
+//     x.style.fontWeight='bold';
+// }
+// function changeFont(e){
+//     let x= document.getElementById("T");
+//     x.style.fontFamily=e.value;
+// }
+// function changeSize(e){
+//     let x= document.getElementById("T");
+//     x.style.fontSize= e.value;
+
+// }
+function funn(){
+    $("#T").css("fontStyle","italic")}
+
+
+
+
+function funnB(){
+    $("#T").css("textDecoration","underline")
+}
+function funnU(){
+    $("#T").css("fontWeight","bold")
+}
